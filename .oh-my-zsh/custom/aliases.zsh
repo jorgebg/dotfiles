e1cf874c18a214039f775a557ff31052f599ea3a
Jorge Barata <jorge.barata@udemy.com>
2015-10-27 10:00:52 +0000

    New aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,6 +3,8 @@ alias ohmyzsh="atom ~/.oh-my-zsh"
 alias tree="tree -C"
 alias curlhead="curl -IL"
 alias http-server="python3 -m http.server"
+alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
+alias rmpyc="find . -name '*.pyc' -delete"
 
 # Docker machines env vars
 alias docker-default-env='eval "$(docker-machine env default)"'
