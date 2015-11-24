b68bfc510649465e9c51f9f683ebb0dcbe6b1f96
Jorge Barata <jorge.barata@udemy.com>
2015-11-24 18:30:06 +0000

    wsclone, loadenvs, dokku client

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,6 +5,20 @@ alias curlhead="curl -IL"
 alias http-server="python3 -m http.server"
 alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
 alias rmpyc="find . -name '*.pyc' -delete"
+alias loadenvs='export $(cat .env | xargs)'
+alias dokku='~/.dokku/contrib/dokku_client.sh'
 
 # Docker machines env vars
 alias docker-default-env='eval "$(docker-machine env default)"'
+
+function wsclone {
+    if [ "$#" -ne 1 ]
+    then
+      echo "Usage: wsclone <USER/PROJECT>"
+      return 1
+    fi
+
+    cd ~/Workspace
+    take $1
+    hub clone $1 .
+}
