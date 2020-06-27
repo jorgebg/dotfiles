45dcc713a8ce606a311b1d65db239b1309efa963
Jorge Barata <jorge.barata.gonzalez@gmail.com>
2020-06-27 16:34:42 +0200

    youtube-dl-album update, apt libs, loadenv renamed, pbcopy and pbpaste commands, infinite scroll for terminator

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -4,11 +4,14 @@ alias dotfiles='eval "$(dotfiles-env)"'
 alias git-search-first-commit='git log --source --all -S'
 alias http-server="python3 -m http.server"
 alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
-alias loadenvs='export $(cat .env | xargs)'
+alias loadenv='export $(cat .env | xargs)'
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias ohmyzsh="$EDITOR ~/.oh-my-zsh"
 alias rmpyc="find . -name '*.pyc' -delete"
 alias sshconfig='cat ~/.ssh/config'
 alias tree="tree -C"
 alias zshrc="$EDITOR ~/.zshrc"
-alias youtube-dl-album="youtube-dl -o '%(playlist_index)2d - %(title)s.%(ext)s' --extract-audio"
+alias youtube-dl-album="youtube-dl -o '%(playlist_index)02d - %(title)s.%(ext)s' --extract-audio --audio-format opus --audio-quality 0 --add-metadata"
+
+if ! command -v pbcopy; then alias pbcopy='xclip -selection clipboard'; fi
+if ! command -v pbpaste; then alias pbpaste='xclip -selection clipboard -o'; fi
